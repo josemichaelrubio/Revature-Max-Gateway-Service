f68ef78aad0aa10b57c7498a13b12cd52500139f
xFore <slhedstrom@gmail.com>
2021-04-29 11:34:58 -0700

    added JWTs.

---
+++ src/main/java/dev/revaturemax/HelloResource.java
@@ -0,0 +1,50 @@
+package dev.revaturemax;
+
+import dev.revaturemax.models.AuthenticationRequest;
+import dev.revaturemax.models.AuthenticationResponse;
+import dev.revaturemax.services.MyUserDetailsService;
+import dev.revaturemax.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @RequestMapping({"/hello"})
+    public String hello(){
+        return "Hello world!";
+    }
+
+    @PostMapping(value="/authenticate")
+    public ResponseEntity<AuthenticationResponse> createAuthenticationRequest(@RequestBody AuthenticationRequest authenticationRequest) throws BadCredentialsException{
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        }catch(BadCredentialsException e){
+            throw new BadCredentialsException("Incorrect username or password", e);
+            }
+        final UserDetails userDetails=userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt=jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
