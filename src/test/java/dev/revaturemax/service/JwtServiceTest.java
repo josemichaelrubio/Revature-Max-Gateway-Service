7dd9339515ee7cfae38e5f317216b093f70bec00
Karl Kanitsch <karl.kanitsch@revature.net>
2021-04-30 10:15:18 -0400

    Switching to dev

--- src/test/java/dev/revaturemax/service/JwtServiceTest.java
+++ src/test/java/dev/revaturemax/service/JwtServiceTest.java
@@ -1,23 +1,39 @@
-package dev.revaturemax.service;
-
-import dev.revaturemax.model.UserAuth;
-import org.junit.jupiter.api.BeforeAll;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static reactor.core.publisher.Mono.when;
-
-@SpringBootTest
-public class JwtServiceTest {
-
-    private String testJWT;
-    private UserAuth userAuth = new UserAuth("username","password","testUser",true,101);
-
-    @BeforeAll
-    public void setup(){
-
-    }
-
-    public void testGenerateToken(){
-        when(System.currentTimeMillis()).thenReturn(1000);
-    }
-}
+//package dev.revaturemax.service;
+//
+//import dev.revaturemax.model.UserAuth;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.Mock;
+//import org.powermock.api.mockito.PowerMockito;
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+//import org.powermock.modules.junit4.PowerMockRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.time.Clock;
+//import java.time.Instant;
+//import java.time.ZoneId;
+//
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest({ Instant.class })
+//@SpringBootTest
+//public class JwtServiceTest {
+//
+//    private String testJWT;
+//    private final UserAuth userAuth = new UserAuth("username","password","testUser",true,101);
+//    private final String instantExpected = "2021-01-01T00:00:00Z";
+//    private final Clock clock = Clock.fixed(Instant.parse(instantExpected), ZoneId.of("UTC"));
+//
+//    @Mock
+//    Instant instant;
+//
+//    @Test
+//    public void testGenerateToken(){
+//        System.out.println(System.getenv("db_url"));
+//        Instant instant = Instant.now(clock);
+//        PowerMockito.mockStatic(Instant.class);
+//        PowerMockito.when(Instant.now()).thenReturn(instant);
+//        Instant now = Instant.now();
+//        Assertions.assertEquals(instantExpected,now.toString());
+//    }
+//}
