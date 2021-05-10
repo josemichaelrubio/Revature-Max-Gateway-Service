effaaa5d792e77d1da4f78a699d5e115dbe3fc5c
Karl Kanitsch <karl.kanitsch@revature.net>
2021-05-10 12:48:18 -0400

    Setting up template for gateway routing

---
+++ src/main/java/dev/revaturemax/controller/BatchController.java
@@ -0,0 +1,30 @@
+package dev.revaturemax.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/batches")
+public class BatchController {
+
+    private static final String BATCH_SERVICE_URL = "http://batch-service/batches";
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Object> getBatchById(@PathVariable("id") long id){
+        String requestURL = BATCH_SERVICE_URL + "/" + id;
+        return restTemplate.exchange(requestURL,HttpMethod.GET,null,Object.class);
+    }
+
+
+
+}
